54b2940a75f7fe5dbfd7489b22a7d40fc6cdbbfb
Thomas Thune Hansen <tth@bellcom.dk>
2014-10-16 10:29:21 +0200

    Updated features, added scripts, removed unused module

---
+++ public_html/profiles/valhalla/modules/valghalla/scripts/remove_old_fields.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @file
+ * remove_old_fields.php
+ *
+ * This script should be run with `drush scr remove_old_fields`.
+ * It removes fields no longer used for various contenttypes.
+ */
+
+// From polling_station nodes.
+if ($instance = field_info_instance('node', 'field_party', 'polling_station')) {
+  field_delete_instance($instance);
+}
+
+if ($instance = field_info_instance('node', 'field_chairman', 'polling_station')) {
+  field_delete_instance($instance);
+}
+
+if ($instance = field_info_instance('node', 'field_volunteers_pr_party', 'polling_station')) {
+  field_delete_instance($instance);
+}
+
+if ($group = field_group_load_field_group('group_parties', 'node', 'polling_station', 'form')) {
+  ctools_include('export');
+  field_group_group_export_delete($group, FALSE);
+}
+
+if ($group = field_group_load_field_group('group_address', 'node', 'polling_station', 'form')) {
+  ctools_include('export');
+  field_group_group_export_delete($group, FALSE);
+}
+
+// From election nodes.
+if ($instance = field_info_instance('node', 'body', 'election')) {
+  field_delete_instance($instance);
+}
+
+// From constituency nodes.
+if ($instance = field_info_instance('node', 'field_ansvarlig', 'constituency')) {
+  field_delete_instance($instance);
+}
+
+if ($instance = field_info_instance('node', 'field_secretary', 'constituency')) {
+  field_delete_instance($instance);
+}
+
+// From roles nodes.
+if ($instance = field_info_instance('node', 'field_diaet', 'roles')) {
+  field_delete_instance($instance);
+}
