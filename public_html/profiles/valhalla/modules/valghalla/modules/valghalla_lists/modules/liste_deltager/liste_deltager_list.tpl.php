8dfd2a766b0e97c64305ec0c0b01fffbb94bb80b
Thomas Thune Hansen <tth@bellcom.dk>
2014-12-10 09:28:49 +0100

    updated list

--- public_html/profiles/valhalla/modules/valghalla/modules/valghalla_lists/modules/liste_deltager/liste_deltager_list.tpl.php
+++ public_html/profiles/valhalla/modules/valghalla/modules/valghalla_lists/modules/liste_deltager/liste_deltager_list.tpl.php
@@ -7,18 +7,20 @@
 <div class="pull-right"><?php print format_date(time(), 'short'); ?></div>
 <br />
 <center>
-  <h4><?php print $header['election'] ?></h4>
+  <h4><?php print $header['election']; ?></h4>
   <h4><?php print $header['date']; ?></h4>
 </center>
 <br /><br />
 
-<h5>
-<?php print $header['polling_station'];?>; <?php print $header['adress'];?>
-<?php if (isset($header['schoolcode'])) : ?>
- - Skolekode <?php print $header['schoolcode'];?>
-<?php endif; ?>
-</h5>
-<h5>
-<?php print $header['contact_info']; ?>
-</h5>
-<?php print $table; ?>
+<?php foreach($table as $polling_station => $markup): ?>
+  <h5>
+  <?php print $header[$polling_station]['polling_station'];?>; <?php print $header[$polling_station]['adress'];?>
+  <?php if (isset($header[$polling_station]['schoolcode'])) : ?>
+  - Skolekode <?php print $header[$polling_station]['schoolcode'];?>
+  <?php endif; ?>
+  </h5>
+  <h5>
+  <?php print $header[$polling_station]['contact_info']; ?>
+  </h5>
+  <?php print $table[$polling_station]; ?>
+<?php endforeach; ?>
