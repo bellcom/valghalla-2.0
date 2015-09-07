5a60dd7672a921405ccea7bc7b0b9c13403fbb60
Morten Nissen <mn@bellcom.dk>
2015-09-07 12:26:23 +0200

    Improve toggle all and change form layout
    - Fixes: #70 toggle all now untoggles to
    - Fixes: #71 improved layout on form to actually use BS3

--- public_html/profiles/valhalla/modules/valghalla/modules/valghalla_lists/modules/liste_deltager/js/liste_deltager.js
+++ public_html/profiles/valhalla/modules/valghalla/modules/valghalla_lists/modules/liste_deltager/js/liste_deltager.js
@@ -1,11 +1,47 @@
 (function ($) {
   Drupal.behaviors.liste_deltager = {
     attach: function (context, settings) {
-      $('.js-select-all').click(function(e){
-        $(this).parent().find('input').prop('checked', true);
-        $(this).parent().find('option').attr('selected','selected');
-        e.preventDefault();
+
+      $('.js-select-all').on('click', function(event) {
+        var allIsSelected = true;
+        var $parent = $(this).parent();
+        var $inputs = $parent.find('input');
+        var $options = $parent.find('option');
+
+        $inputs.each(function(index, value) {
+          var $input = $(this);
+
+          if ( ! $input.is(':checked')) {
+            allIsSelected = false;
+          }
+        });
+
+        $options.each(function(index, value) {
+          var $option = $(this);
+
+          if ( ! $option.is(':selected')) {
+            allIsSelected = false;
+          }
+        });
+
+        // All is checked, so uncheck all
+        if (allIsSelected) {
+          $inputs.prop('checked', false);
+          $options.removeAttr('selected');
+        }
+        // All or some needs to be checked - check all
+        else {
+          $inputs.prop('checked', true);
+          $options.attr('selected', 'selected');
+        }
+
+        event.preventDefault();
       });
+      //$('.js-select-all').click(function(e){
+      //  $(this).parent().find('input').prop('checked', true);
+      //  $(this).parent().find('option').attr('selected', 'selected');
+      //  e.preventDefault();
+      //});
       if ($('.table').length === 0) {
       }
       else {
