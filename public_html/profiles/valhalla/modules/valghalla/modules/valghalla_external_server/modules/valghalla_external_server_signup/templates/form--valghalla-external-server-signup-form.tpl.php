f515267b9cb95ecc677de76bb055d7163c88a1f9
Stanislav Kutasevits <stan@bellcom.dk>
2018-12-05 14:09:39 +0200

    VA-24 code standarts

--- public_html/profiles/valhalla/modules/valghalla/modules/valghalla_external_server/modules/valghalla_external_server_signup/templates/form--valghalla-external-server-signup-form.tpl.php
+++ public_html/profiles/valhalla/modules/valghalla/modules/valghalla_external_server/modules/valghalla_external_server_signup/templates/form--valghalla-external-server-signup-form.tpl.php
@@ -1,23 +1,26 @@
-<h2>
-<!--  --><?php //print $form['#election']; ?>
-</h2>
-
 <p>
   <?php
-    print $form['#upper_text'];
+  print $form['#upper_text'];
   ?>
 </p>
 
 <div style="border: 1px solid black">
   <p>...</p>
+
   <p>NemID block</p>
+
   <p>...</p>
 </div>
 
 <?php
-  $comment_field = $form['comment'];
-  unset($form['comment']);
-  print drupal_render_children($form);
+$comment_field = $form['comment'];
+$submit = $form['submit'];
+$term_agreement = $form['terms_agreement'];
+unset($form['comment']);
+unset($form['submit']);
+unset($form['terms_agreement']);
+
+print drupal_render_children($form);
 ?>
 
 <hr/>
@@ -29,6 +32,7 @@
 </p>
 
 <?php print drupal_render($comment_field); ?>
+<?php print drupal_render($term_agreement); ?>
+<?php print drupal_render($submit); ?>
 
 <?php ?>
-
