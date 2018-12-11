bf8ec0798f5d7303fdb8d141ae3e8c58d0eeda1b
Andriy Iun <andriy.yun@gmail.com>
2018-12-11 14:48:06 +0100

    Rebuild front

--- public_html/profiles/valhalla/themes/custom/site/dist/javascripts/app.js
+++ public_html/profiles/valhalla/themes/custom/site/dist/javascripts/app.js
@@ -8663,10 +8663,10 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 (function ($) {
 
     $(window).load(function () {
-        var input = $('#edit-field-valghalla-mail-attachments-und-0-upload, #edit-field-eboks-attachments-und-0-upload');
+        var input = $('#edit-field-valghalla-mail-attachments-und-0-upload');
         input.addClass('hidden');
 
-        $('#edit-field-valghalla-mail-attachments-und-0-upload-button,#edit-field-eboks-attachments-und-0-upload-button').addClass('hidden');
+        $('#edit-field-valghalla-mail-attachments-und-0-upload-button').addClass('hidden');
         $(":file").filestyle({ buttonText: "Vælg fil" });
 
         $(":file").filestyle({ placeholder: "Ingen fil" });
