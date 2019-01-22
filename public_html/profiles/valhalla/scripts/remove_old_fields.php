c2f5d85e5d23f4064d1da14ba2659687f6fc35fe
Andriy Iun <andriy.yun@gmail.com>
2019-01-22 12:43:41 +0100

    Remove field_volunteers_pr_party from codebase

--- public_html/profiles/valhalla/scripts/remove_old_fields.php
+++ public_html/profiles/valhalla/scripts/remove_old_fields.php
@@ -7,10 +7,6 @@
  * It removes fields no longer used for various contenttypes.
  */
 
-if ($instance = field_info_instance('node', 'field_volunteers_pr_party', 'polling_station')) {
-  field_delete_instance($instance);
-}
-
 if ($group = field_group_load_field_group('group_parties', 'node', 'polling_station', 'form')) {
   ctools_include('export');
   field_group_group_export_delete($group, FALSE);
